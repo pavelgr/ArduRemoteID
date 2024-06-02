12e82170aeab3bf2305f6eb40ffaa1bb0a4dafbf
pavelgr <pavelgr.github@gmail.com>
2024-06-01 23:07:24 -0400

    [pck] Don't regenerate headers for CAN, they currently crash.

--- scripts/regen_headers.sh
+++ scripts/regen_headers.sh
@@ -5,19 +5,19 @@ echo "Generating mavlink2 headers"
 rm -rf libraries/mavlink2/generated
 mavgen.py --wire-protocol 2.0 --lang C modules/mavlink/message_definitions/v1.0/all.xml -o libraries/mavlink2/generated
 
-echo "Generating DroneCAN headers for libcanard"
-rm -rf libraries/DroneCAN_generated
-python3 modules/dronecan_dsdlc/dronecan_dsdlc.py -O libraries/DroneCAN_generated modules/DSDL/uavcan modules/DSDL/dronecan modules/DSDL/com
+#echo "Generating DroneCAN headers for libcanard"
+#rm -rf libraries/DroneCAN_generated
+#python3 modules/dronecan_dsdlc/dronecan_dsdlc.py -O libraries/DroneCAN_generated modules/DSDL/uavcan modules/DSDL/dronecan modules/DSDL/com
 
 # cope with horrible Arduino library handling
-PACKETS="NodeStatus GetNodeInfo HardwareVersion SoftwareVersion RestartNode dynamic_node_id remoteid param Log"
-for p in $PACKETS; do
-    (
-        cd libraries/DroneCAN_generated
-        ln -s include/*"$p"*.h .
-        ln -s src/*"$p"*.c .
-    )
-done
+#PACKETS="NodeStatus GetNodeInfo HardwareVersion SoftwareVersion RestartNode dynamic_node_id remoteid param Log"
+#for p in $PACKETS; do
+#    (
+#        cd libraries/DroneCAN_generated
+#        ln -s include/*"$p"*.h .
+#        ln -s src/*"$p"*.c .
+#    )
+#done
 
 (cd RemoteIDModule && ../scripts/git-version.sh)
 
