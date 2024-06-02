9c9301575060b7932454e7030995cb52ded55d3f
pavelgr <pavelgr.github@gmail.com>
2024-06-01 23:11:46 -0400

    [pck] use platformio.

--- scripts/install_build_env.sh
+++ scripts/install_build_env.sh
@@ -7,6 +7,6 @@ python3 -m pip install pyserial
 python3 -m pip install pexpect
 python3 -m pip install pymonocypher
 
-wget https://downloads.arduino.cc/arduino-cli/arduino-cli_0.27.1_Linux_64bit.tar.gz
-mkdir -p bin
-(cd bin && tar xvzf ../arduino-cli_0.27.1_Linux_64bit.tar.gz)
+#wget https://downloads.arduino.cc/arduino-cli/arduino-cli_0.27.1_Linux_64bit.tar.gz
+#mkdir -p bin
+#(cd bin && tar xvzf ../arduino-cli_0.27.1_Linux_64bit.tar.gz)
